bd7c42b2725b073828dbbf27aa9d150451e85256
Alisha Andersson <alishaanderssonn@icloud.com>
2024-10-23 18:57:58 +0000

    Add Booking model to admin interface

--- SawadeeReservations/admin.py
+++ SawadeeReservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
 
 # Register your models here.
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'email', 'phone_number', 'booking_date', 'number_of_guests', 'created_on', 'updated_on')
+    search_fields = ('customer_name', 'email', 'phone_number')
+    list_filter = ('booking_date',)
+
+admin.site.register(Booking, BookingAdmin)
