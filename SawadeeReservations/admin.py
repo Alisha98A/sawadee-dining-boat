28d1dea7297f23be6cc24f1d911a2f2e5c96dacf
Alisha <alishaanderssonn@icloud.com>
2024-11-04 20:14:35 +0100

    Order bookings by date and exclude past bookings

--- SawadeeReservations/admin.py
+++ SawadeeReservations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Booking
 from .forms import BookingAdminForm
+from django.utils import timezone
 
 # Register your models here.
 class BookingAdmin(admin.ModelAdmin):
@@ -8,5 +9,10 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ('customer_name', 'email', 'phone_number', 'booking_date', 'number_of_guests', 'created_on', 'updated_on')
     search_fields = ('customer_name', 'email', 'phone_number')
     list_filter = ('booking_date',)
+    ordering = ('booking_date',)
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        # Filter to only show upcoming bookings
+        return qs.filter(booking_date__gte=timezone.now())
 admin.site.register(Booking, BookingAdmin)
