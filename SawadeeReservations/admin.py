cfe36c1a408f47fa998b33dc63e2899d338bbc28
Alisha Andersson <alishaanderssonn@icloud.com>
2024-11-15 18:38:57 +0000

    Delete BookingAdminForm

--- SawadeeReservations/admin.py
+++ SawadeeReservations/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Booking
-from .forms import BookingAdminForm
 from django.utils import timezone
 
 # Register your models here.
 class BookingAdmin(admin.ModelAdmin):
-    form = BookingAdminForm
     list_display = ('customer_name', 'email', 'phone_number', 'booking_date', 'number_of_guests', 'created_on', 'updated_on')
     search_fields = ('customer_name', 'email', 'phone_number')
     list_filter = ('booking_date',)
